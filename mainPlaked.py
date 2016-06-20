c4b9e7be790a3cf3e056fe46b3137a51c728092d
Amar Prakash Pandey <amarlearning@users.noreply.github.com>
2016-06-20 14:24:37 +0530

    Update mainPlaked.py

--- mainPlaked.py
+++ mainPlaked.py
@@ -1,1 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: Amar Prakash Pandey
+# @Date:   2016-06-04
+# @Email:  amar.om1994@gmail.com  
+# @Github username: @amarlearning
+# @Last Modified by: Amar Prakash Pandey  
+# @Last Modified date: 2016-06-20
+# MIT License. You can find a copy of the License
+# @http://amarlearning.mit-license.org
 
+import pygames
