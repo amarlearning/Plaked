4e9de93bdd8a37d726508582114620db7e885222
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-06-26 19:44:30 +0530

    bot is up & moving according to key pressed :snake:

--- mainPlaked.py
+++ mainPlaked.py
@@ -12,6 +12,11 @@
 import pygame
 import time
 
+# contant value initialised
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+
 # game initialization done
 pygame.init()
 
@@ -22,13 +27,33 @@ gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Placked | Beyond the Universe')
 pygame.display.update()
 
+# controling the no. of frames per sec uisng pygame clock
+clock = pygame.time.Clock()
+
 # variable init 
 gameExit = False
+start_x = 400
+start_y = 300
+move_to = 0
 
 while not gameExit :
 	for event in pygame.event.get():
 		if event.type  == pygame.QUIT:
 			gameExit = True
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				move_to = -10
+			if event.key == pygame.K_RIGHT:
+				move_to = 10
+
+	start_x += move_to
+
+	gameDisplay.fill(white)
+	pygame.draw.rect(gameDisplay, black, [start_x, start_y , 10, 10])
+	pygame.display.update()
+
+	# initialising no. of frames per sec
+	clock.tick(10)
 
 pygame.quit()
 # you can signoff now, everything looks good!
