3fb05902d93a44ae2d5e52476b221898dc357352
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-06-30 04:07:42 +0530

    gameover and gameexit func added

--- mainPlaked.py
+++ mainPlaked.py
@@ -11,6 +11,7 @@
 # import library here
 import pygame
 import time
+import sys
 
 # contant value initialised
 white = (255,255,255)
@@ -37,47 +38,76 @@ clock = pygame.time.Clock()
 # Frames per second 
 FPS = 10
 
-# variable init 
-gameExit = False
-start_x = display_width/2
-start_y = display_height/2
-move_to_h = 0
-move_to_v = 0
-
 # moving block size
 block = 10
 
-while not gameExit :
-	for event in pygame.event.get():
-		if event.type  == pygame.QUIT:
-			gameExit = True
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				move_to_h = -block
-				move_to_v = 0
-			if event.key == pygame.K_RIGHT:
-				move_to_h = block
-				move_to_v = 0
-			if event.key == pygame.K_UP:
-				move_to_v = -block
-				move_to_h = 0
-			if event.key == pygame.K_DOWN:
-				move_to_v = block
-				move_to_h = 0
-				
-	if start_x >= display_width or start_x < 0 or start_y >= display_height or start_y < 0:
-		gameExit = True
+# init font object with font size 25 
+font = pygame.font.SysFont("cursive", 25)
 
-	start_x += move_to_h
-	start_y += move_to_v
+def message_to_display(msg, color):
+	screen_text = font.render(msg, True, color)
+	gameDisplay.blit(screen_text, [display_width/2-200, display_height/2])
 
-	gameDisplay.fill(white)
-	pygame.draw.rect(gameDisplay, black, [start_x, start_y , block, block])
-	pygame.display.update()
+def gameLoop():
+	# variable init 
+	gameExit = False
+	gameOver = False
 
-	# initialising no. of frames per sec
-	clock.tick(FPS)
+	start_x = display_width/2
+	start_y = display_height/2
+	
+	move_to_h = 0
+	move_to_v = 0
 
-pygame.quit()
-# you can signoff now, everything looks good!
-quit()
+	while not gameExit :
+
+		while gameOver == True :
+			gameDisplay.fill(white)
+			message_to_display("Game Over, Press C to play again or Q to quit!", red)
+			pygame.display.update()
+
+			for event in pygame.event.get():
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_q:
+						gameExit = True
+						gameOver = False
+					if event.key == pygame.K_c:
+						gameLoop()
+
+
+		for event in pygame.event.get():
+			if event.type  == pygame.QUIT:
+				gameExit = True
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					move_to_h = -block
+					move_to_v = 0
+				if event.key == pygame.K_RIGHT:
+					move_to_h = block
+					move_to_v = 0
+				if event.key == pygame.K_UP:
+					move_to_v = -block
+					move_to_h = 0
+				if event.key == pygame.K_DOWN:
+					move_to_v = block
+					move_to_h = 0
+					
+		if start_x >= display_width or start_x < 0 or start_y >= display_height or start_y < 0:
+			gameOver = True
+
+		start_x += move_to_h
+		start_y += move_to_v
+
+		gameDisplay.fill(white)
+		pygame.draw.rect(gameDisplay, black, [start_x, start_y , block, block])
+		pygame.display.update()
+
+		# initialising no. of frames per sec
+		clock.tick(FPS)
+
+
+	pygame.quit()
+	# you can signoff now, everything looks good!
+	quit()
+
+gameLoop()
